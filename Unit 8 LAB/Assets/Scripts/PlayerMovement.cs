2190d78456b2244abe3c5610b676e7c3b77a97e5
BluFox2003 <54640290+BluFox2003@users.noreply.github.com>
2021-02-17 21:55:09 +0000

    Initial Push

---
+++ Unit 8 LAB/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f; //Run speed variable
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump")) //This sees if the Jump key has been pressed
+        {
+            jump = true;
+        }
+        if (Input.GetButtonDown("Crouch")) //This sees if the Crouch key has been pressed
+        {
+            crouch = true;
+
+        }else if (Input.GetButtonUp("Crouch")) //This sees if the Jump key has been let go
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
