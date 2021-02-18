280db62e0b67b1723fbacfa0ff15e5643b40d588
BluFox2003 <54640290+BluFox2003@users.noreply.github.com>
2021-02-18 18:49:04 +0000

    i know im missing a semicolon leave me alone

--- Unit 8 LAB/Assets/Player.cs
+++ Unit 8 LAB/Assets/Player.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [System.Serializable]
     public class PlayerStats{
         public int Health = 10;
     }
+
+    public int fallBoundary = -20;
     
     public PlayerStats playerStats = new PlayerStats();
+
+    void Update (){
+        if (transform.position.y <= fallBoundary)
+            DamagePlayer (9999);
+
+
+    }
     
     public void DamagePlayer(int damage){
         playerStats.Health -= damage;
         if (playerStats.Health <= 0) {
-            Debug.Log("KILL PLAYER");
+            GameMaster.KillPlayer(this);
+
         }
     }
 
--- Unit 8 LAB/Assets/Scripts/GameMaster.cs
+++ Unit 8 LAB/Assets/Scripts/GameMaster.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    public static GameMaster gm;
+
     public static void KillPlayer(Player player) {
         Destroy(player.gameObject);
+        gm.RespawnPlayer();
     }
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+
+    public void RespawnPlayer(){
+        Debug.Log("TODO: Add sound for respawning");
+        Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    void Start(){
+        if (gm == null){
+            gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
+        }
+    }
+    
 }
