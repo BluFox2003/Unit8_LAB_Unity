19e1f5ef2af56afa221f8942130265ecfa16a08f
BluFox2003 <54640290+BluFox2003@users.noreply.github.com>
2021-02-18 16:33:44 +0000

    fusjhfihgsfSF

---
+++ Unit 8 LAB/Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public class PlayerStats{
+        public int Health = 10;
+    }
+    
+    public PlayerStats playerStats = new PlayerStats();
+    
+    public void DamagePlayer(int damage){
+        playerStats.Health -= damage;
+        if (playerStats.Health <= 0) {
+            Debug.Log("KILL PLAYER");
+        }
+    }
+
+}
+
